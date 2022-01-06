13c3f0c6c859de34f6b28ebfc90914e76622241f
Sridhar Ratnakumar <srid@srid.ca>
2022-01-06 17:49:20 -0500

    Store generated CSS in model
    Allows us to directly write in static site generation

--- src/Banyan/View/Common.hs
+++ src/Banyan/View/Common.hs
@@ -7,16 +7,16 @@ import Text.Blaze.Html5 ((!))
 import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as A
 
-routeElemUnlessHere :: Model -> Route -> Either FilePath Route -> H.Html -> H.Html
-routeElemUnlessHere model hereR (r' :: Either FilePath Route) =
-  if r' == Right hereR
+routeElemUnlessHere :: Model -> Route -> SiteRoute -> H.Html -> H.Html
+routeElemUnlessHere model hereR r' =
+  if r' == SRHtml hereR
     then H.a ! A.class_ "font-semibold underline decoration-2 decoration-green-500 decoration-wavy"
     else routeElem model r'
 
-routeElem :: Model -> Either FilePath Route -> H.Html -> H.Html
-routeElem model (r' :: Either FilePath Route) =
+routeElem :: Model -> SiteRoute -> H.Html -> H.Html
+routeElem model r' =
   H.a ! A.class_ "text-green-600 hover:bg-green-500 hover:text-white transition-colors" ! routeHref model r'
 
-routeHref :: Model -> Either FilePath Route -> H.Attribute
+routeHref :: Model -> SiteRoute -> H.Attribute
 routeHref model r' =
   A.href (fromString . toString $ Ema.routeUrl model r')
--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE TemplateHaskell #-}
-
 -- | A very simple site with two routes, and HTML rendered using Blaze DSL
 module Main where
 
@@ -7,10 +5,9 @@ import qualified Banyan.ID as ID
 import Banyan.Model (Model)
 import qualified Banyan.Model as Model
 import qualified Banyan.Model.Patch as Patch
-import Banyan.Route (Route)
+import Banyan.Route
 import qualified Banyan.View as View
 import Control.Lens.Operators ((^.))
-import Control.Monad.Logger (MonadLogger, logInfoN)
 import qualified Data.Map.Strict as Map
 import qualified Ema
 import qualified Ema.CLI
@@ -18,26 +15,7 @@ import qualified Emanote
 import qualified Emanote.Source.Loc as Loc
 import qualified Paths_banyan
 import qualified System.Environment as Env
-import System.FilePath ((</>))
-import System.Process (callProcess)
-import System.Which
 import qualified Test.Tasty as T
-
-tailwind :: FilePath
-tailwind = $(staticWhich "tailwind")
-
-buildCss :: (MonadIO m, MonadLogger m) => Ema.CLI.Action -> m ()
-buildCss action = do
-  logInfoN "Running Tailwind compiler to build style.css"
-  let extraArgs =
-        case action of
-          Ema.CLI.Run -> []
-          _ -> ["--minify"]
-  -- TODO: Time this, since we are not using JIT (-w) mode.
-  liftIO $
-    callProcess
-      tailwind
-      $ ["-i", "input.css", "-o", contentDir </> "style.css"] <> extraArgs
 
 main :: IO ()
 main = do
@@ -58,10 +36,9 @@ contentDir = "content"
 exe :: IO ()
 exe =
   Ema.runEma render $ \act model -> do
-    buildCss act -- Run it once at start, because our HTML is all in Haskell.
     defaultLayer <- Loc.defaultLayer <$> liftIO Paths_banyan.getDataDir
     let layers = one defaultLayer <> Loc.userLayers (one contentDir)
-    model0 <- Model.emptyModel contentDir
+    model0 <- Model.emptyModel act contentDir
     Emanote.emanate
       layers
       (Patch.watching act)
@@ -70,14 +47,16 @@ exe =
       model0
       (\a b -> Patch.patchModel a b . fmap (Loc.locResolve . head))
 
-render :: Ema.CLI.Action -> Model -> Either FilePath Route -> Ema.Asset LByteString
+render :: Ema.CLI.Action -> Model -> SiteRoute -> Ema.Asset LByteString
 render act model = \case
-  Left fp ->
+  SRStatic fp ->
     -- This instructs ema to treat this route "as is" (ie. a static file; no generation)
     -- The argument `fp` refers to the absolute path to the static file.
     case Map.lookup fp (model ^. Model.modelFiles) of
       Nothing -> error "missing static file"
       Just (_, absPath) -> Ema.AssetStatic absPath
-  Right r ->
+  SRCss ->
+    Ema.AssetGenerated Ema.Other $ encodeUtf8 $ model ^. Model.modelCss
+  SRHtml r ->
     -- Generate a Html route; hot-reload is enabled.
     Ema.AssetGenerated Ema.Html $ View.renderHtml act model r
